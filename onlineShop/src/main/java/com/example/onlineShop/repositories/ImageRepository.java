860a0d88444733b0efba9fe33e31601a997cc330
Aliaksei Tutski <tutskialex@gmail.com>
2025-04-27 13:56:37 +0300

    feat: Add functionality for uploading images to products

---
+++ onlineShop/src/main/java/com/example/onlineShop/repositories/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.example.onlineShop.repositories;
+
+import com.example.onlineShop.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
