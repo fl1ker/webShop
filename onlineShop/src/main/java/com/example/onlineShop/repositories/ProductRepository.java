845255b919ba92e7a51701128960e520ef749b2d
Aliaksei Tutski <tutskialex@gmail.com>
2025-04-27 11:56:59 +0300

    feat: Implement product CRUD operations with DB integrations

---
+++ onlineShop/src/main/java/com/example/onlineShop/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.onlineShop.repositories;
+
+import com.example.onlineShop.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
